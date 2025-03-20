86866470a91920694c4a8b834a1e3042eed83f9c
Vladyslav Pidborskyi <vladyslav.pidborskyi@gmail.com>
2025-03-20 11:52:50 +0100

    Remove hardcoded URL from settings for improved configurability

--- core/config.py
+++ core/config.py
@@ -22,9 +22,7 @@ class Settings(BaseSettings):
         env_file=".env", env_file_encoding="utf-8", case_sensitive=True
     )
 
-    URL: str = (
-        "https://www.olx.pl/nieruchomosci/mieszkania/wynajem/warszawa/?search%5Bprivate_business%5D=private&search%5Border%5D=created_at:desc&search%5Bfilter_float_price:to%5D=2500&search%5Bfilter_enum_rooms%5D%5B0%5D=one"
-    )
+    URL: str
 
     DEFAULT_LAST_MINUTES_GETTING: int = 45
     CYCLE_FREQUENCY_SECONDS: int = 5
